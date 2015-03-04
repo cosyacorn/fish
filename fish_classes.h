1e0bac4f32babe04aa090c0fa4149da1a9e9b93d
Shane Harding <sharding@tcd.ie>
2015-03-04 17:30:51 +0000

    initial commit

---
+++ fish_classes.h
@@ -0,0 +1,28 @@
+class minnows{
+ public:
+  int xcoord;
+  int ycoord;
+  int zcoord;
+
+  int age;
+};
+
+class tuna{
+ public:
+  int xcoord;
+  int ycoord;
+  int zcoord;
+
+  int age;
+  int eaten;
+};
+
+class shark{
+ public:
+  int xcoord;
+  int ycoord;
+  int zcoord;
+
+  int age;
+  int eaten;
+};
---
+++ main.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "fish_classes.h"
+
+
+int main(){
+
+  minnows a;
+  a.xcoord=1;
+  a.xcoord++;
+  
+  std::cout << "Hello " << a.xcoord <<  std::endl; 
+
+  return 0;
+}
